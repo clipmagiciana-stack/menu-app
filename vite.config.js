073741d73ee0a38af532141f4dffd1a0fe63d193
clipmagiciana-stack <clipmagician.a@gmail.com>
2026-09-13 08:52:14 +0400

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,4 @@
 import {defineConfig} from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
-export default defineConfig({plugins:[react(),tailwindcss()]});
+export default defineConfig({base:process.env.GITHUB_ACTIONS==='true'?'/'+process.env.GITHUB_REPOSITORY.split('/')[1]+'/':'/',plugins:[react(),tailwindcss()]});
